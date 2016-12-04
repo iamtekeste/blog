bd244e46f2bbc2c778d6d3014009f87ee328abce
Tekeste G <tekeste.kidanu@gmail.com>
2016-12-04 17:25:02 -0500

    remove no longer needed code snippet

--- routes/views/category.js
+++ routes/views/category.js
@@ -16,31 +16,6 @@ exports = module.exports = function (req, res) {
 		categories: [],
 	};
 
-	// Load all categories
-	view.on('init', function (next) {
-
-		keystone.list('PostCategory').model.find().sort('name').exec(function (err, results) {
-
-			if (err || !results.length) {
-				return next(err);
-			}
-
-			locals.data.categories = results;
-
-			// Load the counts for each category
-			async.each(locals.data.categories, function (category, next) {
-
-				keystone.list('Post').model.count().where('categories').in([category.id]).exec(function (err, count) {
-					category.postCount = count;
-					next(err);
-				});
-
-			}, function (err) {
-				next(err);
-			});
-		});
-	});
-
 	// Load the current category filter
 	view.on('init', function (next) {
 
