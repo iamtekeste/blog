e178a8bd8372f8de478a5ab06a02b89b4d7d5deb
Tekeste G <tekeste.kidanu@gmail.com>
2016-12-04 12:22:19 -0500

    add virtual fields to Post model

--- models/Post.js
+++ models/Post.js
@@ -20,13 +20,29 @@ Post.add({
 	content: {
 		brief: { type: Types.Html, wysiwyg: true, height: 150 },
 		extended: { type: Types.Html, wysiwyg: true, height: 400 },
-		youtubeCode: {type: Types.Html, wysiwyg: false, height: 150}
 	},
+	youtubeID: {type: String, required: true, initial: true},
 	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
 });
 
 Post.schema.virtual('content.full').get(function () {
 	return this.content.extended || this.content.brief;
+});
+
+//build post url
+Post.schema.virtual('href').get(function() {
+	return '/post/' + this.slug;
+});
+
+//build video thumbnail url from youtubeID
+Post.schema.virtual('imageURL').get(function() {
+	return 'https://img.youtube.com/vi/' + this.youtubeID + '/hqdefault.jpg';
+});
+
+//build youtube embed code
+Post.schema.virtual('youtubeEmbed').get(function() {
+	var youtubeURL = 'https://www.youtube.com/embed/' + this.youtubeID;
+	return	'<iframe width="850" height="480" src="' + youtubeURL + '" frameborder="0" allowfullscreen></iframe>';
 });
 
 Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
